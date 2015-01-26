d73996ecd69279e32f8cb0757712ee06cd54cf06
miguelperezg <miguelperezg1986@gmail.com>
2015-01-26 12:33:21 +0100

    Subida 2

---
+++ src/main/java/es/open4job/ProyectoServicioTaxi/opendata/App.java
@@ -0,0 +1,13 @@
+package es.open4job.ProyectoServicioTaxi.opendata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
