63aee8ef27eaacfe6dc45904f4aa678b3c800c60
miguelperezg <miguelperezg1986@gmail.com>
2015-01-28 11:10:18 +0100

    Archivo App.java, creación de objetos y consultas a los metodos de cada uno de los DAO correspondientes y cerramos y abrimos conexiones.

--- src/main/java/es/open4job/ProyectoServicioTaxi/opendata/App.java
+++ src/main/java/es/open4job/ProyectoServicioTaxi/opendata/App.java
@@ -1,15 +1,19 @@
 package es.open4job.ProyectoServicioTaxi.opendata;
 
+import java.sql.Connection;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import es.open4job.ProyectoServicioTaxi.opendata.model.dao.EstacionServicioDAO;
 import es.open4job.ProyectoServicioTaxi.opendata.model.dao.ParadaTaxiDAO;
-
-
+import es.open4job.ProyectoServicioTaxi.opendata.model.dao.BaseDatosDAO;
 
 public class App {
-	
+
+	public static final Logger logger = Logger.getLogger(ParadaTaxiDAO.class
+			.getName());
+
 	public static void main(String[] args) {
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String host = "54.154.192.80";
@@ -19,12 +23,28 @@ public class App {
 		String password = "open4job";
 		String url = "jdbc:oracle:thin:" + user + "/" + password + "@" + host
 				+ ":" + puerto + ":" + sid;
-		
-		ParadaTaxiDAO  = new ParadaTaxiDAO(driver,
-				url, user, password);
-		
-		EstacionServicioDAO  = new EstacionServicioDAO(
-				driver, url, user, password);
-		
+
+		BaseDatosDAO bbdd = new BaseDatosDAO(driver, url, user, password);
+		int id = 3;
+		try {
+			Connection conexion = bbdd.abrirConexion();
+			
+			
+			ParadaTaxiDAO paradaTaxi = new ParadaTaxiDAO();
+			paradaTaxi.getListadoParadaTaxi(conexion);
+			paradaTaxi.getparadataxi(id, conexion);
+			
+			EstacionServicioDAO estacionServicio = new EstacionServicioDAO();
+			estacionServicio.getlistaServicio(conexion);
+			estacionServicio.getEstacionServicio(id, conexion);
+			
+		} catch (ClassNotFoundException e) {
+			logger.log(Level.SEVERE,
+					"ClassNotFoundException : " + e.getMessage());
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE, "SQLException : " + e.getMessage());
+		}
+		bbdd.cerrarConexion();
+
 	}
 }
